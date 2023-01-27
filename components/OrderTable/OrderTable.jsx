473193a4feea8f074a7a61e04b48c14fbb22b181
Agamjot Singh Jashan <tippytactics@gmail.com>
2023-01-27 21:05:47 +0530

    added search

--- components/OrderTable/OrderTable.jsx
+++ components/OrderTable/OrderTable.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/OrderTable.module.css";
-import swr from "swr";
 import Link from "next/link";
 export default function OrderTable() {
   // FETCHING ORDERS
-  const fetcher = (url) => fetch(url).then((res) => res.json());
-  const { data: orders, error: ordersErr } = swr(
-    process.env.NEXT_PUBLIC_API + "/api/orders",
-    fetcher
-  );
+
+  useEffect(() => {
+    // I used useEffect here to easily asign the data to the state
+    fetch(process.env.NEXT_PUBLIC_API + "/api/orders")
+      .then((res) => res.json())
+      .then((data) => setOrderState(data));
+  }, []);
+
+  // i used state here to implement the search functionality
+
+  const [orderState, setOrderState] = useState(null);
+
+  const handleSearch = (e) => {
+    const searchValue = e.target.value;
+    if (searchValue) {
+      // serach the state for the value in order description or id
+      const filteredOrders = orderState.filter((order) => {
+        if (
+          order.orderDescription.includes(searchValue) ||
+          order.id == searchValue
+        ) {
+          return order;
+        }
+      });
+      setOrderState(filteredOrders);
+    } else {
+      // if the search value is empty, fetch the orders again
+      fetch(process.env.NEXT_PUBLIC_API + "/api/orders")
+        .then((res) => res.json())
+        .then((data) => setOrderState(data));
+    }
+  };
 
   return (
     <div className={styles.orderTable}>
-      <h1>Order Management</h1>
+      <div className={styles.orderTableHead}>
+        <h1>Order Management</h1>
+        <input
+          type="search"
+          placeholder="Search by order description or id"
+          onChange={handleSearch}
+        />
+      </div>
       <table>
         <thead>
           <tr>
@@ -24,8 +57,8 @@ export default function OrderTable() {
           </tr>
         </thead>
         <tbody>
-          {orders &&
-            orders.map((order) => (
+          {orderState &&
+            orderState.map((order) => (
               <tr key={order.id}>
                 <td>{order.id}</td>
                 <td>{order.orderDescription}</td>
